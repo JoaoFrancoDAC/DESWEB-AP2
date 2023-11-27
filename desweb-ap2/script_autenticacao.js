79d7e09aa40bd174d2fd6336d9e21dbb80df5557
Joao Victor Franco da Costa <142028379+JoaoFrancoDAC@users.noreply.github.com>
2023-11-27 05:46:39 -0300

    Delete desweb-ap2 directory

--- desweb-ap2/script_autenticacao.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const valorEsperado = 'senha';
-    const valorNoStorage = localStorage.getItem('senhaUsuario');
-
-    if (valorNoStorage !== valorEsperado) {
-        window.location.href = 'index.html';
-    }
-});
